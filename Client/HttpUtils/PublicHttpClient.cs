83adb604c384d4a048bbba5ef55696909cf12290
b1ankk <ttemk22@gmail.com>
2021-07-27 19:56:27 +0200

    Add PublicHttpClient for accessing public APIs
    Regular HttpClient supplied with BaseAddressAuthorizationMessageHandler will fail when authorisation is not required.

---
+++ Client/HttpUtils/PublicHttpClient.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Http;
+
+namespace ExpensesApp.Client.HttpUtils {
+    
+    public class PublicHttpClient {
+        public HttpClient Client { get; }
+        
+        public PublicHttpClient(HttpClient client) {
+            Client = client;
+        }
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,13 +1,10 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
+using ExpensesApp.Client.HttpUtils;
 
 namespace ExpensesApp.Client
 {
@@ -18,9 +15,15 @@ namespace ExpensesApp.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddHttpClient("ExpensesApp.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
-                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
-
+            builder.Services.AddHttpClient(
+                "ExpensesApp.ServerAPI", 
+                client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+            ).AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+            
+            builder.Services.AddHttpClient<PublicHttpClient>(
+                client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) 
+            );
+            
             // Supply HttpClient instances that include access tokens when making requests to the server project
             builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("ExpensesApp.ServerAPI"));
 
