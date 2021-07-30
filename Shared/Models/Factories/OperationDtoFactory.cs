ebd4d4a63eb486c3cb889bc1ee13676330faac38
b1ankk <ttemk22@gmail.com>
2021-07-30 14:57:58 +0200

    Add OperationDto and factory classes parsing DTO to/from Operation

---
+++ Shared/Models/Factories/OperationDtoFactory.cs
@@ -0,0 +1,21 @@
+﻿using ExpensesApp.Shared.Models.DTOs;
+
+namespace ExpensesApp.Shared.Models.Factories
+{
+    public static class OperationDtoFactory
+    {
+        public static OperationDto CreateFromOperation(Operation operation)
+        {
+            return new OperationDto {
+                IdOperation = operation.IdOperation,
+                OperationDate = operation.OperationDate,
+                TransactionDate = operation.TransactionDate,
+                TransactionType = operation.TransactionType,
+                Currency = operation.Currency,
+                Amount = operation.Amount,
+                AfterOperationBalance = operation.AfterOperationBalance,
+                Description = operation.Description
+            };
+        }
+    }
+}
---
+++ Shared/Models/Factories/OperationFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ExpensesApp.Shared.Models.DTOs;
+
+namespace ExpensesApp.Shared.Models.Factories
+{
+    public static class OperationFactory
+    {
+        public static Operation CreateFromOperationDto(OperationDto operationDto)
+        {
+            return new Operation {
+                IdOperation = operationDto.IdOperation ?? throw new NullReferenceException(),
+                OperationDate = operationDto.OperationDate ?? throw new NullReferenceException(),
+                TransactionDate = operationDto.TransactionDate ?? throw new NullReferenceException(),
+                TransactionType = operationDto.TransactionType,
+                Currency = operationDto.Currency,
+                Amount = operationDto.Amount ?? throw new NullReferenceException(),
+                AfterOperationBalance = operationDto.AfterOperationBalance ?? throw new NullReferenceException(),
+                Description = operationDto.Description
+            };
+        }
+    }
+}
