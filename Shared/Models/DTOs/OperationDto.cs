6c8c96e652015da56cba4694058c5d78cae01ffd
b1ankk <ttemk22@gmail.com>
2021-08-01 16:04:41 +0200

    Make nullable values in OperationDto not nullable to fit model

--- Shared/Models/DTOs/OperationDto.cs
+++ Shared/Models/DTOs/OperationDto.cs
@@ -5,13 +5,13 @@ namespace ExpensesApp.Shared.Models.DTOs
 {
     public class OperationDto
     {
-        public int? IdOperation { get; set; }
-        public DateTime? OperationDate { get; set; }
-        public DateTime? TransactionDate { get; set; }
+        public int IdOperation { get; set; }
+        public DateTime OperationDate { get; set; }
+        public DateTime TransactionDate { get; set; }
         public string TransactionType { get; set; }
-        public decimal? Amount { get; set; }
+        public decimal Amount { get; set; }
         public string Currency { get; set; }
-        public decimal? AfterOperationBalance { get; set; }
+        public decimal AfterOperationBalance { get; set; }
         public string Description { get; set; }
     }
 }
