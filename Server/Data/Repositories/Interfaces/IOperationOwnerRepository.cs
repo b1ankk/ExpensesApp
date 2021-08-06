b5ee56d28483bea881c1b21846e36f87b3b16274
b1ankk <ttemk22@gmail.com>
2021-08-06 21:35:31 +0200

    Inherit missing IRepository<> interface in implemented repositories

--- Server/Data/Repositories/Interfaces/IOperationOwnerRepository.cs
+++ Server/Data/Repositories/Interfaces/IOperationOwnerRepository.cs
@@ -1,4 +1,7 @@
-﻿namespace ExpensesApp.Server.Data.Repositories.Interfaces
+﻿using ExpensesApp.Server.Data.Repositories.Base;
+using ExpensesApp.Shared.Models;
+
+namespace ExpensesApp.Server.Data.Repositories.Interfaces
 {
-    public interface IOperationOwnerRepository { }
+    public interface IOperationOwnerRepository : IRepository<OperationOwner> { }
 }
--- Server/Data/Repositories/Interfaces/IOperationTypeRepository.cs
+++ Server/Data/Repositories/Interfaces/IOperationTypeRepository.cs
@@ -1,4 +1,7 @@
-﻿namespace ExpensesApp.Server.Data.Repositories.Interfaces
+﻿using ExpensesApp.Server.Data.Repositories.Base;
+using ExpensesApp.Shared.Models;
+
+namespace ExpensesApp.Server.Data.Repositories.Interfaces
 {
-    public interface IOperationTypeRepository { }
+    public interface IOperationTypeRepository : IRepository<OperationType> { }
 }
