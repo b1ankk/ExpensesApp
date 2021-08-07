19db8e6b03807b83810ce63c624ecb4f6be18725
b1ankk <ttemk22@gmail.com>
2021-08-07 12:41:39 +0200

    Add foreign keys to OperationDto

--- Shared/Models/DTOs/OperationDto.cs
+++ Shared/Models/DTOs/OperationDto.cs
@@ -1,11 +1,11 @@
 ﻿using System;
-using System.Numerics;
 
 namespace ExpensesApp.Shared.Models.DTOs
 {
     public class OperationDto
     {
         public int IdOperation { get; set; }
+        
         public DateTime OperationDate { get; set; }
         public DateTime TransactionDate { get; set; }
         public string TransactionType { get; set; }
@@ -13,5 +13,8 @@ namespace ExpensesApp.Shared.Models.DTOs
         public string Currency { get; set; }
         public decimal AfterOperationBalance { get; set; }
         public string Description { get; set; }
+        
+        public int? IdOperationType { get; set; }
+        public int? IdOperationOwner { get; set; }
     }
 }
--- Shared/Models/DTOs/VerboseOperationDto.cs
+++ Shared/Models/DTOs/VerboseOperationDto.cs
@@ -2,9 +2,6 @@
 {
     public class VerboseOperationDto : OperationDto
     {
-        public int? IdOperationType { get; set; }
-        public int? IdOperationOwner { get; set; }
-        
         public OperationTypeDto OperationType { get; set; }
         public OperationOwnerDto OperationOwner { get; set; }
     }
