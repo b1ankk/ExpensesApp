d0e864796701dbd1d51e0233d8afba02b9c618df
b1ankk <ttemk22@gmail.com>
2021-08-07 13:47:02 +0200

    Add utility method for copying object's properties to another object

---
+++ Shared/Utility/Objects.cs
@@ -0,0 +1,19 @@
+﻿namespace ExpensesApp.Shared.Utility
+{
+    public static class Objects
+    {
+        public static void SetProperties(this object destination, object source) {
+            CopyProperties(source, destination);
+        }
+        
+        public static void CopyProperties(object source, object destination) {
+            var destProperties = destination.GetType().GetProperties();
+            
+            foreach (var destProperty in destProperties) {
+                var sourceProperty = source.GetType().GetProperty(destProperty.Name);
+                if (sourceProperty != null)
+                    destProperty.SetValue(destination, sourceProperty.GetValue(source));
+            }
+        }
+    }
+}
