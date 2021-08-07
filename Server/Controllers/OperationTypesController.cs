00d61ab49f49e512221716bbfe3670c9f4e9d7cb
b1ankk <ttemk22@gmail.com>
2021-08-07 14:51:35 +0200

    Add OperationTypeController and GET endpoint

---
+++ Server/Controllers/OperationTypesController.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using ExpensesApp.Server.Data.UnitOfWork;
+using ExpensesApp.Shared.AutoMapperExtensions;
+using ExpensesApp.Shared.Constants;
+using ExpensesApp.Shared.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpensesApp.Server.Controllers
+{
+    [ApiController]
+    [Route(Paths.Api.OperationTypes)]
+    public class OperationTypesController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        
+        public OperationTypesController(IUnitOfWork unitOfWork, IMapper mapper) {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        
+        
+        [HttpGet]
+        public async Task<IActionResult> GetOperationTypes() {
+            var operationTypes = await _unitOfWork.OperationTypes.GetAllAsync();
+            
+            var operationTypeDtos = _mapper.MapAll<OperationTypeDto>(operationTypes);
+            
+            return Ok(operationTypeDtos);
+        }
+        
+    }
+}
